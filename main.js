2843278483a527383414b1a0e478863ef67f5ee7
oche0003 <oche0003@algoinquinlive.com>
2023-12-10 20:07:46 -0500

    adds more styles to css

--- main.js
+++
@@ -1,8 +0,0 @@
-// Toggle responsive menu
-function toggleMenu() {
-    const nav = document.querySelector('nav');
-    nav.classList.toggle('show');
-}
-
-// Add an event listener to the toggle button
-document.querySelector('.toggle-btn').addEventListener('click', toggleMenu);
